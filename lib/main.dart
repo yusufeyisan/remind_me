1244921c12031cd29a2328d142cec4d34ef6f340
yusuf <yusuf@endpoint-labs.com>
2019-05-05 23:23:18 +0300

    Update primary color code

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 import './screens/home.dart';
 import './screens/add_word.dart';
 import './screens/word_list.dart';
+
 void main() => runApp(RemindMe());
 
 class RemindMe extends StatelessWidget {
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -16,28 +16,30 @@ class RemindMe extends StatelessWidget {
     );
   }
 
+   static Color primaryColor = new Color(0xFFFFCE00);
+
   final ThemeData themeData = ThemeData(
-        brightness: Brightness.light,
-        primaryColor: Colors.yellow,
-        accentColor: Colors.white,
-        scaffoldBackgroundColor: Colors.white,
-        textSelectionHandleColor: Colors.black,
-        textSelectionColor: Colors.black12,
-        cursorColor: Colors.black,
-        toggleableActiveColor: Colors.black,
-        inputDecorationTheme: InputDecorationTheme(
-          border: const OutlineInputBorder(
-            borderSide: BorderSide(color: Colors.black),
-          ),
-          enabledBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: Colors.black.withOpacity(0.1)),
-          ),
-          focusedBorder: const OutlineInputBorder(
-            borderSide: BorderSide(color: Colors.black),
-          ),
-          labelStyle: const TextStyle(
-            color: Colors.black,
-          ),
-        ),
-      );
+    brightness: Brightness.light,
+    primaryColor: primaryColor,
+    accentColor: Colors.white,
+    scaffoldBackgroundColor: Colors.white,
+    textSelectionHandleColor: Colors.black,
+    textSelectionColor: Colors.black12,
+    cursorColor: Colors.black,
+    toggleableActiveColor: Colors.black,
+    inputDecorationTheme: InputDecorationTheme(
+      border: const OutlineInputBorder(
+        borderSide: BorderSide(color: Colors.black),
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: Colors.black.withOpacity(0.1)),
+      ),
+      focusedBorder: const OutlineInputBorder(
+        borderSide: BorderSide(color: Colors.black),
+      ),
+      labelStyle: const TextStyle(
+        color: Colors.black,
+      ),
+    ),
+  );
 }
