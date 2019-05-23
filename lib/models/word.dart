34d498e08b1189c85465bbbf6b57fe93ea1850bb
yusuf <yusuf@endpoint-labs.com>
2019-05-24 01:29:54 +0300

    Update word model

--- lib/models/word.dart
+++ lib/models/word.dart
@@ -1,10 +1,11 @@
 class Word {
+  int _id;
   String _word;
   String _first;
   String _second;
   String _third;
   String _synonyms;
-  bool _active;
+  int _active;
   int _priority;
 
 // constracture
@@ -20,12 +21,13 @@ class Word {
 
   // set model fields
   Word.map(dynamic obj) {
+    this._id = obj["_id"];
     this._word = obj["word"];
     this._first = obj["first"];
     this._second = obj["second"];
     this._third = obj["third"];
     this._synonyms = obj["synonyms"];
-    this._active = (obj["active"]) == 1 ? true : false;
+    this._active = obj["active"];
     this._priority = obj["priority"];
   }
 
@@ -35,20 +37,43 @@ class Word {
   String get second => _second;
   String get third => _third;
   String get synonyms => _synonyms;
-  bool get active => _active;
+  int get active => _active;
   int get priority => _priority;
 
   // model to map
   Map<String, dynamic> toMap() {
     var map = Map<String, dynamic>();
-
+    map["_id"] = _id;
     map["word"] = _word;
     map["first"] = _first;
     map["second"] = _second;
     map["third"] = _third;
     map["synonyms"] = _synonyms;
-    map["active"] = _active ? 1 : 0;
+    map["active"] = _active;
     map["priority"] = _priority;
     return map;
+  }
+
+  Word.fromJson(Map json)
+      : _id = json['_id'],
+        _word = json['word'],
+        _first = json['first'],
+        _second = json['second'],
+        _third = json['third'],
+        _synonyms = json['synonyms'],
+        _active = json['active'],
+        _priority = json['priority'];
+
+  Map toJson() {
+    return {
+      '_id': _id,
+      'word': _word,
+      'first': _first,
+      'second': _second,
+      'third': _third,
+      'synonyms': _synonyms,
+      'active': _active,
+      'priority': _priority,
+    };
   }
 }
