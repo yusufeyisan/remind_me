a6ea9b3cb3f9c651580d7fc86f9af472b2342f40
yusuf <yusuf@endpoint-labs.com>
2019-05-27 01:34:41 +0300

    Add db connection

--- lib/screens/word_list.dart
+++ lib/screens/word_list.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:remind_me/models/word.dart';
 import '../data/database_helper.dart';
+import '../screens/edit_word.dart';
 
 class WordListPage extends StatefulWidget {
   @override
@@ -16,22 +19,7 @@ class _WordListPageState extends State<WordListPage> {
   final TextStyle synonymStyle =
       TextStyle(fontSize: 15, fontStyle: FontStyle.italic, letterSpacing: 1);
 
-  List<Word> items = [
-    //new Words("Happy", "Mutlu", "Neşeli", "Keyifli", false),
-    Word("Dream", "Rüya", "Düş", "Hayal", "delusion", 1, 1),
-    Word("Dream", "Rüya", "Düş", "Hayal", "delusion", 1, 1),
-    Word("Dream", "Rüya", "Düş", "Hayal", "delusion", 1, 1),
-    Word("Dream", "Rüya", "Düş", "Hayal", "delusion", 1, 1),
-    Word("Dream", "Rüya", "Düş", "Hayal", "delusion", 1, 1),
-    Word("Dream", "Rüya", "Düş", "Hayal", "delusion", 1, 1),
-    Word("Dream", "Rüya", "Düş", "Hayal", "delusion", 1, 1),
-    Word("Dream", "Rüya", "Düş", "Hayal", "delusion", 1, 1),
-    Word("Dream", "Rüya", "Düş", "Hayal", "delusion", 1, 1),
-    Word("Dream", "Rüya", "Düş", "Hayal", "delusion", 1, 1),
-    Word("Dream", "Rüya", "Düş", "Hayal", "delusion", 1, 1),
-    Word("Dream", "Rüya", "Düş", "Hayal", "delusion", 1, 1),
-    Word("Dream", "Rüya", "Düş", "Hayal", "delusion", 1, 1)
-  ];
+  List<Word> items = [];
 
   @override
   Widget build(BuildContext context) {
@@ -57,83 +45,182 @@ class _WordListPageState extends State<WordListPage> {
             ];
           },
           body: new ListView.builder(
-              itemCount: items.length,
-              itemBuilder: (BuildContext ctxt, int index) {
-                return new Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  mainAxisSize: MainAxisSize.max,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Padding(
-                      padding: EdgeInsets.all(10),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+              itemCount: getWordCount(),
+              itemBuilder: (BuildContext context, int index) {
+                return Dismissible(
+                    direction: (items[index].active == 0
+                        ? DismissDirection.horizontal
+                        : DismissDirection.startToEnd),
+                    background: Container(
+                      alignment: Alignment.centerLeft,
+                      padding: EdgeInsets.only(left: 20.0),
+                      color: Colors.orangeAccent,
+                      child: Row(
                         children: <Widget>[
-                          Row(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Text(
-                                "Word: ${items[index].word}",
-                                style: wordStyle,
-                              ),
-                            ],
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            mainAxisSize: MainAxisSize.max,
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            children: <Widget>[
-                              Text(
-                                "Means: ${items[index].first}\t-\t${items[index].second}\t-\t${items[index].third}",
-                                style: descriptionStyle,
-                              ),
-                            ],
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            mainAxisSize: MainAxisSize.max,
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            children: <Widget>[
-                              Text(
-                                "Synonym: ${items[index].synonyms}",
-                                style: synonymStyle,
-                              ),
-                            ],
-                          )
+                          Icon(Icons.edit, color: Colors.white),
+                          SizedBox(width: 10),
+                          Text("Do you want to edit it?",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w500))
                         ],
                       ),
                     ),
-                    Column(
+                    secondaryBackground: Container(
+                      alignment: Alignment.centerRight,
+                      padding: EdgeInsets.only(right: 20.0),
+                      color: Colors.redAccent,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: <Widget>[
+                          Text("Do you want to delete it?",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w500)),
+                          SizedBox(width: 10),
+                          Icon(Icons.delete, color: Colors.white)
+                        ],
+                      ),
+                    ),
+                    key: new Key(items[index].word),
+                    dragStartBehavior: DragStartBehavior.down,
+                    confirmDismiss: (direction) async {
+                      final bool res = await showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              title: const Text("Confirm"),
+                              content: direction.index == 2
+                                  ? Text(
+                                      "Are you sure you wish to delete this item?")
+                                  : Text(
+                                      "Are you sure you wish to edit this item?"),
+                              actions: <Widget>[
+                                RaisedButton(
+                                    onPressed: () =>
+                                        Navigator.of(context).pop(true),
+                                    child: direction.index == 2
+                                        ? Text("DELETE")
+                                        : Text("EDIT")),
+                                RaisedButton(
+                                    onPressed: () =>
+                                        Navigator.of(context).pop(false),
+                                    child: const Text("CANCEL"))
+                              ],
+                            );
+                          });
+                      return res;
+                    },
+                    onDismissed: (direction) {
+                      if (direction.index == 2) {
+                        print("delete");
+                        delete(index);
+                        Scaffold.of(context).showSnackBar(new SnackBar(
+                          content: new Text("Word deleted successfully"),
+                        ));
+                      } else {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => EditWordPage(
+                                  wordObject: items[index],
+                                ),
+                          ),
+                        );
+                        print("length: " + items.length.toString());
+                        print("index: " + index.toString());
+                        print("name: " + items[index].word);
+                        setState(() {
+                          items.remove(items[index]);
+                        });
+                        query();
+                      }
+                    },
+                    child: new Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      mainAxisSize: MainAxisSize.max,
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
-                        Checkbox(
-                          value: items[index].active == 1 ? true : false,
-                          activeColor: Colors.green,
-                          onChanged: (b) {
-                            _update(index, (b ? 1 : 0));
-                            _query();
-                          },
+                        Padding(
+                          padding: EdgeInsets.all(10),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  Text(
+                                    "Word: ${items[index].word}",
+                                    style: wordStyle,
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                mainAxisSize: MainAxisSize.max,
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                children: <Widget>[
+                                  Text(
+                                    "Means: ${items[index].first}\t-\t" +
+                                        "${items[index].second}\t-\t" +
+                                        "${items[index].third}",
+                                    style: descriptionStyle,
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                mainAxisSize: MainAxisSize.max,
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                children: <Widget>[
+                                  Text(
+                                    "Synonym: ${items[index].synonyms}",
+                                    style: synonymStyle,
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                        Column(
+                          children: <Widget>[
+                            Checkbox(
+                              value: items[index].active == 1 ? true : false,
+                              activeColor: Colors.green,
+                              onChanged: (b) {
+                                // update function takes index and value of active
+                                update(index, (b ? 1 : 0));
+                              },
+                            )
+                          ],
                         )
                       ],
-                    )
-                  ],
-                );
+                    ));
               })),
     );
   }
 
-  void _query() async {
+  @override
+  void initState() {
+    super.initState();
+    query();
+  }
+
+  void query() async {
     final allRows = await dbHelper.queryAllRows();
     setState(() {
       items = allRows.map((word) => Word.fromJson(word)).toList();
     });
   }
 
-  void _update(int id, int active) async {
+  void update(int id, int active) async {
     var word = items[id];
-    var index = id++;
+
     // row to update
     Map<String, dynamic> row = {
-      DatabaseHelper.columnId: index,
+      DatabaseHelper.columnId: word.id,
       DatabaseHelper.columnWord: word.word,
       DatabaseHelper.columnFirst: word.first,
       DatabaseHelper.columnSecond: word.second,
@@ -144,12 +231,18 @@ class _WordListPageState extends State<WordListPage> {
     };
     final rowsAffected = await dbHelper.update(row);
     print('updated $rowsAffected row(s)');
+    query();
   }
 
-  void _delete() async {
+  void delete(int id) async {
     // Assuming that the number of rows is the id for the last row.
-    final id = await dbHelper.queryRowCount();
     final rowsDeleted = await dbHelper.delete(id);
     print('deleted $rowsDeleted row(s): row $id');
+    query();
+  }
+
+  // return length of items array
+  int getWordCount() {
+    return items.length;
   }
 }
